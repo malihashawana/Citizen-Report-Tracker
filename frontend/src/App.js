62a28c65950ebe9946f713c8cac3bc57e39f9ca7
malihashawana <shawana.maliha@g.bracu.ac.bd>
2025-11-30 22:11:34 +0600

    setup frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Citizen Report Tracker</h1>
+      <p>Frontend setup successful.</p>
+    </div>
+  );
+}
+
+export default App;
